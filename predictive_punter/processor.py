82b1d32efdd3187eafa276eb94286aedf903f607
Jason Green <justjasongreen@gmail.com>
2016-07-22 22:27:29 +1000

    Implement tests.processor.process_date_test.test_meets

--- predictive_punter/processor.py
+++ predictive_punter/processor.py
@@ -1,19 +1,34 @@
 class Processor:
     """Abstract class to implement processing of racing entities in a standardised way"""
 
-    @property
-    def must_process_dates(self):
-        """Return True if this processor instance must process dates"""
+    def __init__(self, provider, *args, **kwargs):
 
-        return hasattr(self, 'pre_process_date') or hasattr(self, 'post_process_date')
+        self.provider = provider
+
+    @property
+    def must_process_meets(self):
+        """Return True if this processor instance must process meets or any other associated entities"""
+
+        return hasattr(self, 'pre_process_meet') or hasattr(self, 'post_process_meet')
 
     def process_date(self, date):
-        """Process all racing data for the specified date"""
+        """Process the specified date"""
 
-        if self.must_process_dates:
+        if hasattr(self, 'pre_process_date'):
+            self.pre_process_date(date)
 
-            if hasattr(self, 'pre_process_date'):
-                self.pre_process_date(date)
+        if self.must_process_meets:
+            for meet in self.provider.get_meets_by_date(date):
+                self.process_meet(meet)
 
-            if hasattr(self, 'post_process_date'):
-                self.post_process_date(date)
+        if hasattr(self, 'post_process_date'):
+            self.post_process_date(date)
+
+    def process_meet(self, meet):
+        """Process the specified meet"""
+
+        if hasattr(self, 'pre_process_meet'):
+            self.pre_process_meet(meet)
+
+        if hasattr(self, 'post_process_meet'):
+            self.post_process_meet(meet)
--- tests/processor/process_date_test.py
+++ tests/processor/process_date_test.py
@@ -1,6 +1,14 @@
 from datetime import datetime
 
+import cache_requests
+from lxml import html
 import predictive_punter
+import punters_client
+import pymongo
+import pytest
+import racing_data
+import redis
+import requests
 
 
 class CountingProcessor(predictive_punter.Processor):
@@ -12,8 +20,8 @@ class CountingProcessor(predictive_punter.Processor):
 
         self.counter = {}
 
-        self.pre_process_date = self.pre_process_entity
-        self.post_process_date = self.post_process_entity
+        self.pre_process_date = self.pre_process_meet = self.pre_process_entity
+        self.post_process_date = self.post_process_meet = self.post_process_entity
 
     def increment_counter(self, entity, phase):
         """Increment the counter for the entity type"""
@@ -39,10 +47,43 @@ class CountingProcessor(predictive_punter.Processor):
         self.increment_counter(entity, 'post')
 
 
-def test_dates():
-    """The process_date method should call the pre/post_process_date method the expected number of times"""
+@pytest.fixture(scope='module')
+def processor():
 
-    processor = CountingProcessor()
+    database_uri = 'mongodb://localhost:27017/predictive_punter_test'
+    database_name = database_uri.split('/')[-1]
+    database_client = pymongo.MongoClient(database_uri)
+    database_client.drop_database(database_name)
+    database = database_client.get_default_database()
+
+    http_client = None
+    try:
+        http_client = cache_requests.Session(connection=redis.fromurl('redis://localhost:6379/predictive_punter_test'))
+    except BaseException:
+        try:
+            http_client = cache_requests.Session()
+        except BaseException:
+            http_client = requests.Session()
+
+    html_parser = html.fromstring
+
+    scraper = punters_client.Scraper(http_client, html_parser)
+
+    provider = racing_data.Provider(database, scraper)
+
+    processor = CountingProcessor(provider)
     processor.process_date(datetime(2016, 2, 1))
 
+    return processor
+
+
+def test_dates(processor):
+    """The process_date method should call the pre/post_process_date method the expected number of times"""
+
     assert processor.counter[datetime]['pre'] == processor.counter[datetime]['post'] == 1
+
+
+def test_meets(processor):
+    """The process_date method should call the pre/post_process_meet method the expected number of times"""
+
+    assert processor.counter[racing_data.Meet]['pre'] == processor.counter[racing_data.Meet]['post'] == 2
