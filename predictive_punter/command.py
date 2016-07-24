ae4bc414f3bb0069ef861c1389fa549c78cc1548
Jason Green <justjasongreen@gmail.com>
2016-07-24 13:44:03 +1000

    Implement logging

--- predictive_punter/command.py
+++ predictive_punter/command.py
@@ -12,6 +12,7 @@ import redis
 import requests
 
 from .date_utils import *
+from .profiling_utils import *
 
 
 class Command:
@@ -23,7 +24,7 @@ class Command:
 
         config = cls.parse_args(args)
         command = cls(**config)
-        command.process_dates(config['date_from'], config['date_to'])
+        log_time('processing dates from {date_from:%Y-%m-%d} to {date_to:%Y-%m-%d}'.format(date_from=config['date_from'], date_to=config['date_to']), command.process_dates, config['date_from'], config['date_to'])
 
     @classmethod
     def parse_args(cls, args):
@@ -34,10 +35,11 @@ class Command:
             'database_uri':     'mongodb://localhost:27017/predictive_punter',
             'date_from':        datetime.now(),
             'date_to':          datetime.now(),
+            'logging_level':    logging.INFO,
             'redis_uri':        'redis://localhost:6379/predictive_punter'
         }
 
-        opts, args = getopt(args, 'bd:r:', ['backup-database', 'database-uri=', 'redis-uri='])
+        opts, args = getopt(args, 'bd:qr:v', ['backup-database', 'database-uri=', 'quiet', 'redis-uri=', 'verbose'])
 
         for opt, arg in opts:
 
@@ -47,8 +49,14 @@ class Command:
             elif opt in ('-d', '--database-uri'):
                 config['database_uri'] = arg
 
+            elif opt in ('-q', '--quiet'):
+                config['logging_level'] = logging.WARNING
+
             elif opt in ('-r', '--redis-uri'):
                 config['redis_uri'] = arg
+
+            elif opt in ('-v', '--verbose'):
+                config['logging_level'] = logging.DEBUG
 
         if len(args) > 0:
             config['date_from'] = config['date_to'] = datetime.strptime(args[-1], '%Y-%m-%d')
@@ -58,6 +66,8 @@ class Command:
         return config
 
     def __init__(self, *args, **kwargs):
+
+        logging.basicConfig(level=kwargs['logging_level'])
 
         database_client = pymongo.MongoClient(kwargs['database_uri'])
         self.database = database_client.get_default_database()
@@ -102,7 +112,7 @@ class Command:
 
             def process_item(item, retry_count=0, max_retries=5):
                 try:
-                    return executor.submit(target, item)
+                    return executor.submit(log_time, 'processing {item}'.format(item=item), target, item)
                 except RuntimeError:
                     if retry_count < max_retries:
                         time.sleep(1)
@@ -120,7 +130,7 @@ class Command:
         """Process all racing data for the specified date range"""
 
         for date in dates(date_from, date_to):
-            self.process_date(date)
+            log_time('processing date {date:%Y-%m-%d}'.format(date=date), self.process_date, date)
 
     def process_date(self, date):
         """Process all racing data for the specified date"""
@@ -129,11 +139,12 @@ class Command:
             self.process_collection(self.provider.get_meets_by_date(date), self.process_meet)
 
         except BaseException:
-            self.restore_database()
+            logging.critical('An unhandled exception occurred while processing date {date:%Y-%m-%d}'.format(date=date))
+            log_time('restoring database from backup', self.restore_database)
             raise
 
         else:
-            self.backup_database()
+            log_time('backing up database', self.backup_database)
 
     def process_meet(self, meet):
         """Process the specified meet"""
@@ -148,12 +159,26 @@ class Command:
     def process_runner(self, runner):
         """Process the specified runner"""
 
-        self.process_horse(runner.horse)
-
-        runner.jockey
-        runner.trainer
+        log_time('processing {horse}'.format(horse=runner.horse), self.process_horse, runner.horse)
+        log_time('processing {jockey}'.format(jockey=runner.jockey), self.process_jockey, runner.jockey)
+        log_time('processing {trainer}'.format(trainer=runner.trainer), self.process_trainer, runner.trainer)
 
     def process_horse(self, horse):
         """Process the specified horse"""
 
-        horse.performances
+        self.process_collection(horse.performances, self.process_performance)
+
+    def process_jockey(self, jockey):
+        """Process the specified jockey"""
+
+        pass
+
+    def process_trainer(self, trainer):
+        """Process the specified trainer"""
+
+        pass
+
+    def process_performance(self, performance):
+        """Process the specified performance"""
+
+        pass
---
+++ predictive_punter/profiling_utils.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+import logging
+
+
+def log_time(message, target, *target_args, **target_kwargs):
+    """Log the start/elapsed time at the beginning/end of target execution"""
+
+    start_time = datetime.now()
+    logging.info('Started {message} at {start_time}'.format(message=message, start_time=start_time))
+
+    output = target(*target_args, **target_kwargs)
+
+    elapsed_time = datetime.now() - start_time
+    logging.info('Finished {message} in {elapsed_time}'.format(message=message, elapsed_time=elapsed_time))
+
+    return output
